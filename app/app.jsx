c1aad1037d4cb783bf3af2828570bc468426f12a
Gabriel Yakub <gabrielyakub@gmail.com>
2017-06-04 14:45:41 +0700

    Add navigation and routing

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
@@ -17,7 +19,8 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-
+      <IndexRoute component={Timer}></IndexRoute>
+      <Route path="/countdown" component={Countdown}></Route>
     </Route>
   </Router>,
   document.getElementById('app')
---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+var Countdown = React.createClass({
+  render: function(){
+    return (<div>
+      Countdown.jsx
+    </div>)
+  }
+});
+
+module.exports = Countdown;
