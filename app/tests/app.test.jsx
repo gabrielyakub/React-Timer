9bd1154eabe19ae8fc889afb3999594d7bd96a31
Gabriel Yakub <gabrielyakub@gmail.com>
2017-06-04 14:15:33 +0700

    Add automatic tests

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,13 @@
+var expect = require('expect');
+
+
+
+
+// describe buat format output biar lebih readable
+describe('App', ()=>{
+  //ini syntax mochajs -> framework buat testing
+  it('should properly run tests', () => {
+    // isi testnya yang harus terpenuhi
+    expect(1).toBe(1);
+  });
+});
