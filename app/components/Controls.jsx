2b035cf43bb9d7d988e3c0c104fb7e40aa97d3a2
Gabriel Yakub <gabrielyakub@gmail.com>
2017-06-09 21:42:37 +0700

    Add controls

---
+++ app/components/Controls.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+
+var Controls = React.createClass({
+  propTypes: {
+    countdownStatus: React.PropTypes.string.isRequired
+  },
+  render: function(){
+    var {countdownStatus} = this.props;
+
+    var renderStartStopButton = () => {
+      if(countdownStatus === 'started'){
+        return <button className="button secondary">Pause</button>
+      }else{
+        return <button className="button primary">Start</button>
+      }
+    };
+
+    return (
+      <div className="controls">
+        <button className="button alert hollow">Clear</button>
+        {renderStartStopButton()}
+      </div>
+    )
+  }
+});
+
+
+module.exports = Controls;
