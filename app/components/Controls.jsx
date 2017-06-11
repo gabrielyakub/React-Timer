de810f1bcdc54037afde2b85b537066105c887fb
Gabriel Yakub <gabrielyakub@gmail.com>
2017-06-11 13:21:27 +0700

    Wire up controls component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -3,23 +3,33 @@ var React = require('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string.isRequired
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
+  },
+  onStatusChange: function (newStatus){
+    // pake kyk gini soalnya di render gk boleh update state sema sekali,
+    //  harus 'pure function'
+    return () => {
+      this.props.onStatusChange(newStatus);
+    }
   },
   render: function(){
     var {countdownStatus} = this.props;
 
     var renderStartStopButton = () => {
       if(countdownStatus === 'started'){
-        return <button className="button secondary">Pause</button>
+        // on click nge expect function buat di jalanin, disini kita pake function
+        // buat nge return function
+        return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
       }else{
-        return <button className="button primary">Start</button>
+        return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
 
     return (
       <div className="controls">
-        <button className="button alert hollow">Clear</button>
         {renderStartStopButton()}
+        <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
     )
   }
